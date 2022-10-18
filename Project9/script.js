8e04dfbedb3673e99c1b5291333e2c89da8c9e65
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-19 03:55:08 +0500

    Created index.html & script.js files for fav num to be print
    Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.

---
+++ Project9/script.js
@@ -0,0 +1,7 @@
+/* Favorite Number: Store your favorite number in a variable. 
+Then, using that variable, create a message that reveals
+your favorite number. Print that message. */
+let fav_num = 7;
+// template literals
+let message = `${fav_num}`;
+console.log(message);
