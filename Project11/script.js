d20324f6b2c60e7e8a8125870e5a21a5777a6dac
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-19 04:13:23 +0500

    Arrays
    Created index.html & script.js files for
    Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.

---
+++ Project11/script.js
@@ -0,0 +1,8 @@
+// Arrays
+let names = ["Yasir", "Ali", "Rizwan", "Wajid", "Fahad"];
+// Print each element of an array one at a time
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
