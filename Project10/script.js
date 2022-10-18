73dbad0271a6b0b3f752ca435a9a72d1fa02c0ad
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-19 04:05:59 +0500

    Comments
    Created index.html & script.js for single & multi lines comments
    Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.

---
+++ Project10/script.js
@@ -0,0 +1,5 @@
+// Enter user name
+var userName = "Muhammad Aun";
+/* This is multi line comment
+& here we are printing userName with addition text */
+console.log(userName,"is a Web 3.0 & Metaverse student");
