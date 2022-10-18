021c7f3ca140b4cb4c43a64a45582ceb7c28b009
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-19 04:21:09 +0500

    Arrays with message
    Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Project12/script.js
@@ -0,0 +1,8 @@
+// Arrays
+let names = ["Yasir", "Ali", "Rizwan", "Wajid", "Fahad"];
+// Print each element of an array one at a time with the same message
+console.log(names[0], "Congratulations on the task completion");
+console.log(names[1], "Congratulations on the task completion");
+console.log(names[2], "Congratulations on the task completion");
+console.log(names[3], "Congratulations on the task completion");
+console.log(names[4], "Congratulations on the task completion");
