c303e4badf78b64866b7dd3f23b6f30d3ad910aa
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-19 03:42:03 +0500

    Add sub multiply & divide
    Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.

---
+++ Project7/script.js
@@ -0,0 +1,13 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+let num1 = prompt("Enter number to add");
+let sum = num1+8;
+console.log(sum);
+let num2 = prompt("Enter number to subtract");
+let sub = num2-8;
+console.log(sub);
+let num3 = prompt("Enter number to multiply");
+let multiply = num3*8;
+console.log(multiply);
+let num4 = prompt("Enter number to divide");
+let divide = num4/8;
+console.log(divide);
