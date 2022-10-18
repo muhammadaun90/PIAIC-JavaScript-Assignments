de125e4096abbbfa804b65e4ec4334a899aae3ad
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-19 02:43:41 +0500

    Project4 quote
    Created index.html file for quote project

---
+++ Project4/script.js
@@ -0,0 +1,3 @@
+let prophet = "The Holy Prophet pbuh";
+let quote = `${prophet} said: The things by which my Ummah can enjoy Heaven, most of all are godliness and a good temper.`;
+console.log(quote);
