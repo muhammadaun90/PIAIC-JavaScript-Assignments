7f4c4a87955fef5ac7c9c9026b5acec3c8a9a32a
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-23 19:01:32 +0500

    Sorting & reversing an arrays
    Created an index.html & script.js files for sorting & reversing an arrays.

---
+++ Project18/script.js
@@ -0,0 +1,11 @@
+// Store atleast 5 places in the world you'd like to visit
+let myVisits = ["Canada", "Australia", "Germany", "Finland", "Italy"];
+console.log(myVisits);
+// Sorting alphabetically
+let sortedMyVisits = myVisits.sort();
+console.log(sortedMyVisits);
+console.log(myVisits);
+myVisits.reverse(myVisits);
+console.log(myVisits);
+myVisits.sort();
+console.log(myVisits);
