fd2dc0e33a7113b32199db4ca1f5195fcb949517
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-23 19:22:34 +0500

    Created index.html & script.js
    Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.

---
+++ Project20/script.js
@@ -0,0 +1,24 @@
+// A list of mountains, rivers, countries, cities, languages
+let listOne = [{
+    mountains: "K2",
+    rivers: "Indus River",
+    countries: "Pakistan",
+    cities: "Karachi",
+    languages: "Urdu"
+},
+{
+    mountains: "Shishapangma",
+    rivers: "Yangtze River",
+    countries: "China",
+    cities: "Beijing",
+    languages: "Chinese"
+},
+{
+    mountains: "Cradle",
+    rivers: "Murray River",
+    countries: "Australia",
+    cities: "Sydney",
+    languages: "English"
+}
+];
+console.log(listOne);
