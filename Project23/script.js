3908cf912864fcd439ff6ab9bfe51d8e3653f9cb
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-23 20:04:43 +0500

    Create index.html & script.js files for Conditional tests.
    Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.

---
+++ Project23/script.js
@@ -0,0 +1,45 @@
+// Contional tests
+// Creating 10 tests
+let car = "subaru";
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+let car1 = "Suzuki";
+console.log("Is car == 'BMW'? I predict False");
+console.log(car1 == 'BMW');
+
+let car2 = "BMW";
+console.log("Is car == 'BMW'? I predict True");
+console.log(car2 == 'BMW');
+
+let car3 = "Lamborghni";
+console.log("Is car == 'Lamborghni'? I predict true");
+console.log(car3 == 'Lamborghni');
+
+let car4 = "Suzuki Cultus";
+console.log("Is car == 'Suzuki Mehran'? I predict False");
+console.log(car4 == 'Suzuki Mehran');
+
+let car5 = "Suzuki Mehran";
+console.log("Is car == 'Suzuki Mehran'? I predict true");
+console.log(car5 == 'Suzuki Mehran');
+
+let car6 = "Ferrari";
+console.log("Is car == 'KIA Classic'? I predict false");
+console.log(car6 == 'KIA Classic');
+
+let car7 = "Honda";
+console.log("Is car == 'Mira'? I predict False");
+console.log(car7 == 'Mira');
+
+let car8 = "KIA";
+console.log("Is car == 'KIA'? I predict true");
+console.log(car8 == 'KIA');
+
+let car9 = "Mitsubishi";
+console.log("Is car == 'BMW'? I predict False");
+console.log(car9 == 'BMW');
+
+let car10 = "Toyota";
+console.log("Is car == 'Toyota'? I predict true");
+console.log(car10 == 'Toyota');
