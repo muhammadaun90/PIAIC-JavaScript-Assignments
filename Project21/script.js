0e6bf108b8007d9f8e3c3a51299c002420dd6be7
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-23 19:32:44 +0500

    Create index.html & script.js files
    They think of something you could store in a JavaScript Object. Write a program that creates Objects containing these items.

---
+++ Project21/script.js
@@ -0,0 +1,24 @@
+let persons = [
+    {
+        userName: "Aun",
+        Age: 32,
+        address: "Gulshan e Iqbal",
+        city: "Karachi",
+        country: "Pakistan"
+    },
+    {
+        userName: "Ahmed",
+        Age: 24,
+        address: "Ravi",
+        city: "Lahore",
+        country: "Pakistan"
+    },
+    {
+        userName: "Zeeshan",
+        Age: 28,
+        address: "Potts Point",
+        city: "Sydney",
+        country: "Australia"
+    }
+];
+console.log(persons);
