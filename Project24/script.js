795bcbcd9b12b7a0fee0d21bbe91422a3ef1fb9c
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-23 20:31:09 +0500

    Project 24 Create index.html & script.js files for more conditional ternary operators tests
    Project 24 More Conditional Tests
    You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests.

---
+++ Project24/script.js
@@ -0,0 +1,29 @@
+/* More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+
+• Tests for equality and inequality with strings
+
+• Tests using the lower case function
+
+• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+• Tests using "and" and "or" operators
+
+• Test whether an item is in a array
+
+• Test whether an item is not in a array */
+
+let humanbeings = "Human Rights"
+humanbeings ? console.log("Equality"): console.log("Inequality");
+
+let userName = "AUN";
+userName == userName.toLowerCase() ? console.log(true) : console.log(false);
+userName == userName.toUpperCase() ? console.log(true) : console.log(false);
+
+
+let age = 18;
+age > 18 ? console.log("denied") : console.log("allowed");
+
+let userAge = 25;
+userAge >= 30 ? console.log(true) : console.log(false);
+userAge <= 25 ? console.log(true) : console.log(false);
+userAge < 25 ? console.log(true) : console.log(false);
