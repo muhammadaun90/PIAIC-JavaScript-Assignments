2596a1ea465d993277fdb41c99b8f62abceb0bfd
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-27 03:00:37 +0500

    Update script.js file
    Some changes made in script.js file

--- Project24/script.js
+++ Project24/script.js
@@ -12,18 +12,34 @@
 
 • Test whether an item is not in a array */
 
+// for equality & inequality with strings
 let humanbeings = "Human Rights"
 humanbeings ? console.log("Equality"): console.log("Inequality");
 
+// for lower & upper case functions
 let userName = "AUN";
 userName == userName.toLowerCase() ? console.log(true) : console.log(false);
-userName == userName.toUpperCase() ? console.log("Yes it's a upper case") : console.log("Yes it's not a upper case");
+userName == userName.toUpperCase() ? console.log("Yes it's a upper case") : console.log("It's not a upper case");
 
-
+// numerical tests
 let age = 18;
 age < 18 ? console.log("allowed") : console.log("denied");
 
 let userAge = 25;
 userAge >= 30 ? console.log(true) : console.log(false);
 userAge <= 25 ? console.log(true) : console.log(false);
-userAge < 25 ? console.log(true) : console.log(false);
+userAge < 25 ? console.log("Your age is equals to 25") : console.log("Your age is not equals to 25");
+
+// for || operators
+let usersAge = prompt("Enter your age");
+let userMarks = prompt("Enter your marks");
+userAge < 17 || userMarks < 80 ? console.log("You are eligible for the course") : console.log("You are not eligible");
+
+// test for && operators
+let yourAge = prompt("Enter your age");
+let nationality = prompt("Your nationality");
+yourAge < 17 && nationality == "Pakistani" ? console.log("You are allowed") : console.log("Not allowed");
+
+let fruits = ["Mango", "Banana", "Orange", "Pineapple", "Apple"];
+fruits[4] ? console.log("Available") : console.log("Not available");
+fruits[6] ? console.log("Available") : console.log("Not available");
